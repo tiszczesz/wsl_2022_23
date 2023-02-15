f5e8e2db3ead00759286d9bd7644ec8cde83674b
tiszczesz <tiszczesz@gmail.com>
2023-02-15 11:07:35 +0100

    gg

---
+++ 4tisp_webowe/cw1-first-v1/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Component1Component } from './component1/component1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Component1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
