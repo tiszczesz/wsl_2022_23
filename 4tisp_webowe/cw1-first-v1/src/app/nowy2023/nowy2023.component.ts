6064be2d49284315e09116840353d6a4a5c2abc1
tiszczesz <tiszczesz@gmail.com>
2023-02-16 11:05:09 +0100

    gg

--- 4tisp_webowe/cw1-first-v1/src/app/nowy2023/nowy2023.component.ts
+++ 4tisp_webowe/cw1-first-v1/src/app/nowy2023/nowy2023.component.ts
@@ -10,11 +10,18 @@ export class Nowy2023Component {
   firstname = "Adam";
   lastname = "Nowak";
   users = ["Jan","Adam","Monika","Eliza","Franek","Łukasz"];
-  MyUsers = [
-    new User("Adam","Nowak",45),
-    new User("Monika","Frankowiak",41),
-    new User("Tomasz","Bomasz",22),
-    new User("Eryk","Wałecki",32),
-  ];
+  MyUsers:Array<User>;
+
+  constructor(){
+    this.MyUsers = [
+      new User("Adam","Nowak",45),
+      new User("Monika","Frankowiak",41),
+      new User("Tomasz","Bomasz",22),
+      new User("Eryk","Wałecki",32),
+      new User("Renata","Tymon",22),
+      new User("Angelika","Grodzka",34),
+    ];
+  }
+  
 
 }
