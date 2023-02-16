036251b2abdf07d47f0d39ea46bc1eb1cb41b541
tiszczesz <tiszczesz@gmail.com>
2023-02-16 12:21:53 +0100

    gg

--- 4tisp_webowe/cw1-first-v1/src/app/app.module.ts
+++ 4tisp_webowe/cw1-first-v1/src/app/app.module.ts
@@ -6,17 +6,19 @@ import { AppComponent } from './app.component';
 import { Component1Component } from './component1/component1.component';
 import { Nowy2023Component } from './nowy2023/nowy2023.component';
 import { Comp2Component } from './comp2/comp2.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     Component1Component,
     Nowy2023Component,
-    Comp2Component
+    Comp2Component,    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 4tisp_webowe/cw1-first-v1/src/app/comp2/comp2.component.ts
+++ 4tisp_webowe/cw1-first-v1/src/app/comp2/comp2.component.ts
@@ -8,6 +8,11 @@ import { Component } from '@angular/core';
 export class Comp2Component {
   text1 = "To jest tekst 1";
     text2: string;
+    text3 = "to jest tekst do innerHTML";
+    red = "red";
+    imgUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Giewont_a2.jpg/440px-Giewont_a2.jpg"
+    text4 = "to będzie zmienione";
+    
     constructor(){
       this.text2 = "to jest tekst 2";
     }
