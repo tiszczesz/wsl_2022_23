e02f7d8279c6938b3a05024d2537105668b8e681
tiszczesz <tiszczesz@gmail.com>
2023-02-23 12:31:20 +0100

    gg

--- 4tisp_webowe/cw1-first-v1/src/app/comp2/comp2.component.ts
+++ 4tisp_webowe/cw1-first-v1/src/app/comp2/comp2.component.ts
@@ -6,14 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./comp2.component.css']
 })
 export class Comp2Component {
+
+
   text1 = "To jest tekst 1";
-    text2: string;
-    text3 = "to jest tekst do innerHTML";
-    red = "red";
-    imgUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Giewont_a2.jpg/440px-Giewont_a2.jpg"
-    text4 = "to będzie zmienione";
-    
-    constructor(){
-      this.text2 = "to jest tekst 2";
-    }
+  text2: string;
+  text3 = "to jest tekst do innerHTML";
+  red = "red";
+  imgUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Giewont_a2.jpg/440px-Giewont_a2.jpg"
+  text4 = "to będzie zmienione";
+  currentDate: any;
+  isBorder:Boolean = false;
+
+  constructor() {
+    this.text2 = "to jest tekst 2";
+    this.updateDate();
+  }
+  updateDate() {
+    setInterval(() => {
+      this.currentDate = new Date().toLocaleTimeString();
+    }, 1000);
+  }
+  OnClick() {
+    console.log("Kliknięto");
+  }
+  ChangeBorder(event:Event) {
+    console.log( (event.target as HTMLImageElement).src);
+    this.isBorder = !this.isBorder;
+  }
 }
