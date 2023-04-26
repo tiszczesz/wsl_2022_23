1dfe68032e2b519327a197cd43e07ec70e1b48a3
tiszczesz <tiszczesz@gmail.com>
2023-04-26 11:34:50 +0200

    gg

--- 4tisp_webowe/cw5-material/src/app/app-routing.module.ts
+++ 4tisp_webowe/cw5-material/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemAddComponent } from './item-add/item-add.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'add', component:ItemAddComponent},
+  {path:'list', component:ItemListComponent},
+  {path:"**", component: MainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- 4tisp_webowe/cw5-material/src/app/app.module.ts
+++ 4tisp_webowe/cw5-material/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatButtonModule} from '@angular/material/button'; 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { ItemAddComponent } from './item-add/item-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ItemListComponent,
+    ItemAddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
